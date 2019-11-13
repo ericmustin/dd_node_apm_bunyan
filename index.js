c3ef671718f867197cf94218b79011fd3f735846
ericmustin <mustin.eric@gmail.com>
2019-11-13 11:45:45 +0100

    demonstrate winston logging instead of bunyan

--- index.js
+++ index.js
@@ -3,16 +3,28 @@ const tracer = require('dd-trace').init({
 });
 const axios = require('axios');
 const express = require('express');
-const bunyan = require('bunyan'); 
-const log = bunyan.createLogger({ 
-  name: 'general-display-web',
-  streams: [
-    {
-      level: 'info',
-      path: 'logs.json'
-    }
-  ] 
+const winston = require('winston');
+const log = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'example-service' },
+  transports: [
+    new winston.transports.File({ filename: 'logs.json' })
+  ]
 });
+
+
+
+// const bunyan = require('bunyan'); 
+// const log = bunyan.createLogger({ 
+//   name: 'general-display-web',
+//   streams: [
+//     {
+//       level: 'info',
+//       path: 'logs.json'
+//     }
+//   ] 
+// });
 
 // Comment out this section to see via terminal instead
 
